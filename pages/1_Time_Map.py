f0f19aad223f42893c987a0bea7ee0c019da6c7e
Krzy-888 <kmacz.888@gmail.com>
2025-10-14 00:24:46 +0200

    Labels bug fixed i hope

--- pages/1_Time_Map.py
+++ pages/1_Time_Map.py
@@ -14,21 +14,21 @@ st.set_page_config(
 st.text("**First Time Interval**")
 col1, col2 = st.columns(2)
 with col1:
-    lat_1 = st.number_input('lat:')
+    lat_1 = st.number_input('lat 1:')
 with col2:
-    lon_1 = st.number_input('lon')
+    lon_1 = st.number_input('lon 1:')
 st.text("**Second Time Interval**")
 col3, col4 = st.columns(2)
 with col3:
-    lat_2 = st.number_input('lat:')
+    lat_2 = st.number_input('lat 2:')
 with col4:
-    lon_2 = st.number_input('lon')
+    lon_2 = st.number_input('lon 2:')
 st.text("**Third Time Interval**")
 col5, col6 = st.columns(2)
 with col5:
-    lat_3 = st.number_input('lat:')
+    lat_3 = st.number_input('lat 3:')
 with col6:
-    lon_3 = st.number_input('lon')
+    lon_3 = st.number_input('lon 3:')
 Options = ['Stremlit', 'Folium', 'Leaflet']
 map_type = st.selectbox("**Select type:**",Options)
 
